83a6fa18529e9941b59b003230953fd1e4040d6a
Sint Sint Hlaing <sints2001@gmail.com>
2023-03-09 15:21:19 +0630

    Fix song details lyrics

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -24,17 +24,25 @@ const SongDetails = () => {
   return (
     <div className="flex flex-col">
       <DetailsHeader artistId={artistId} songData={songData} />
+
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+
         <div className="mt-5">
-          {songData?.sections[1]?.text.map((line, i) => (
-            <p
-              key={`lyrics-${line}-${i}`}
-              className="text-gray-400 text-base my-1"
-            >
-              {line}
+          {songData?.sections[1].type === "LYRICS" ? (
+            songData?.sections[1]?.text.map((line, i) => (
+              <p
+                key={`lyrics-${line}-${i}`}
+                className="text-gray-400 text-base my-1"
+              >
+                {line}
+              </p>
+            ))
+          ) : (
+            <p className="text-gray-400 text-base my-1">
+              Sorry, No lyrics found!
             </p>
-          ))}
+          )}
         </div>
       </div>
     </div>
